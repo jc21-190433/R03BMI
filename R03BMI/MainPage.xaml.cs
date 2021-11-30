1dc74e99a5876670a710e86a60b9fa1f34a83738
敬幸 平山 <190433@jc-21.jp>
2021-11-30 12:33:15 +0900

    名前を自分の名前に

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -13,11 +13,29 @@ namespace R03BMI
         public MainPage()
         {
             InitializeComponent();
-            creater.Text = "JK3A00 吉澤　毅";
+            creater.Text = "JK3A32 平山　敬幸";
         }
+
+
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            double h, w, answer;
+            h = double.Parse(heighit.Text);
+            if (h > 3)
+            {
+                h = h / 100;
+            }
+            w = double.Parse(weighit.Text);
+            if (w > 1000)
+            {
+                w = w / 100;
+            }
+
+
+
+            answer = w / (h * h);
+            result.Text = "" + answer;
 
         }
     }
